7c3802690fffd9c81c3d26f6caa5e76bf2150715
raviranjan mishra <raviranjanmishra767@gmail.com>
2025-12-16 21:18:21 +0530

    upload all image

---
+++ src/assets/images/Allimage.js
@@ -0,0 +1,8 @@
+import img1 from "./err.png"
+import img2 from "./github.png"
+import img3 from "./Gmail.png"
+import img4 from "./linkdin.png"
+import fireimage from "./fire.svg"
+
+const images = [img1,img2,img3,img4,fireimage];
+export default images
